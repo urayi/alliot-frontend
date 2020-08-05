78f131d600794203e7257c374f4cbd24e8cf2de1
Jose Polanco <urayi@URAYI-NITRO5.localdomain>
2020-08-05 04:29:41 -0400

    Configuraciones para los servicios REST

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import axios from 'axios';
+
+// Configuración Inicial de peticiones a los servicios
+axios.defaults.baseURL = 'http://localhost:3000' || process.env.BASE_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
+axios.defaults.headers.common = {
+  'Access-Control-Allow-Origin': '*'
+}
+// Interceptor para incluir el token en las peticiones 
+axios.interceptors.request.use(function (config) {
+  config.headers.authorization = localStorage.getItem('token') ? localStorage.getItem('token') : '';
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
 
 ReactDOM.render(
   <React.StrictMode>
